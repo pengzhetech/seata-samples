ef90d67339609dcab039903539b796cd26c9edee
Tyrion <zhe.peng@lazada.com>
2019-08-10 11:34:52 +0800

    springboot-dubbo-seata

--- springboot-dubbo-seata/samples-business/src/main/java/io/seata/samples/integration/call/controller/BusinessController.java
+++ springboot-dubbo-seata/samples-business/src/main/java/io/seata/samples/integration/call/controller/BusinessController.java
@@ -7,14 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 /**
  * @Author: heshouyou
- * @Description  Dubbo业务执行入口
+ * @Description Dubbo业务执行入口
  * @Date Created in 2019/1/14 17:15
  */
 @RestController
@@ -29,13 +28,42 @@ public class BusinessController {
     private BusinessService businessService;
 
     /**
+     * http://127.0.0.1:8104//business/dubbo/buy
+     * <p>
+     * <p>
+     * {
+     * "userId":"1",
+     * "commodityCode":"C201901140001",
+     * "name":"伟大的哲哥",
+     * "count":10,
+     * "amount":10
+     * <p>
+     * }
+     * <p>
      * 模拟用户购买商品下单业务逻辑流程
+     *
      * @Param:
      * @Return:
      */
     @PostMapping("/buy")
-    ObjectResponse handleBusiness(@RequestBody BusinessDTO businessDTO){
-        LOGGER.info("请求参数：{}",businessDTO.toString());
+    ObjectResponse handleBusiness(@RequestBody BusinessDTO businessDTO) {
+        LOGGER.info("请求参数：{}", businessDTO.toString());
         return businessService.handleBusiness(businessDTO);
     }
+
+    /**
+     * http://127.0.0.1:8104//business/dubbo/getBuy
+     * @return
+     */
+    @GetMapping("/getBuy")
+    ObjectResponse handleGetBusiness() {
+        BusinessDTO businessDTO = new BusinessDTO();
+        businessDTO.setUserId("1");
+        businessDTO.setAmount(BigDecimal.TEN);
+        businessDTO.setCommodityCode("C201901140001");
+        businessDTO.setCount(10);
+        businessDTO.setName("哲哥");
+        return businessService.handleBusiness(businessDTO);
+    }
+
 }
