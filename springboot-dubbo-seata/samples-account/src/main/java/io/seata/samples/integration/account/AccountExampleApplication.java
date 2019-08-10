1e6bac968f6922bad0b0de91e443f111762f9fe7
Tyrion <zhe.peng@lazada.com>
2019-08-10 11:16:00 +0800

    springboot-dubbo-seata

--- springboot-dubbo-seata/samples-account/src/main/java/io/seata/samples/integration/account/AccountExampleApplication.java
+++ springboot-dubbo-seata/samples-account/src/main/java/io/seata/samples/integration/account/AccountExampleApplication.java
@@ -1,8 +1,9 @@
 package io.seata.samples.integration.account;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,7 +14,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDubbo(scanBasePackages = "io.seata.samples.integration.account")
 public class AccountExampleApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountExampleApplication.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting~~~~~~~~~~~~~~~~");
         SpringApplication.run(AccountExampleApplication.class, args);
     }
 
--- springboot-dubbo-seata/samples-account/src/main/java/io/seata/samples/integration/account/controller/TAccountController.java
+++ springboot-dubbo-seata/samples-account/src/main/java/io/seata/samples/integration/account/controller/TAccountController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -34,6 +31,11 @@ public class TAccountController {
     ObjectResponse decreaseAccount(@RequestBody AccountDTO accountDTO) {
         LOGGER.info("请求账户微服务：{}", accountDTO.toString());
         return accountService.decreaseAccount(accountDTO);
+    }
+
+    @GetMapping("/test")
+    String test() {
+        return "OK";
     }
 }
 
